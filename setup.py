025e9d555ec297d70ce618a43e7366f713c86a6c
Josh Douch <me@joshdouch.me>
2025-01-13 17:12:45 +0000

    Fixed SimpleMailboxTable

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='netbox-services',
-    version='0.2',
+    version='0.2.1',
     description='Netbox Services NetBox plugin',
     install_requires=[],
     packages=find_packages(),
